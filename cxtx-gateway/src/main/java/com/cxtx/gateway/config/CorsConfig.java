0c09c4682e6f0ce86d862f3d33b9b0431b62e368
sliu <1622436253@qq.com>
2021-07-09 10:53:20 +0800

    取出cros配置

--- cxtx-gateway/src/main/java/com/cxtx/gateway/config/CorsConfig.java
+++ cxtx-gateway/src/main/java/com/cxtx/gateway/config/CorsConfig.java
@@ -8,24 +8,26 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 /**
  * spring gateway配置cros
+ * 后台配置cros 会产生而外的请求
+ * 前端先发送一个options 请求，后台返回值，前端根据值判断具体的请求是否允许跨域，然后再发送具体的请求
  */
-@Configuration
-public class CorsConfig {
-
-
-	@Bean
-	public CorsWebFilter corsFilter(){
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-		source.registerCorsConfiguration("/**", buildConfig());
-		return new CorsWebFilter(source);
-	}
-
-	private CorsConfiguration buildConfig(){
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		//在生产环境上最好指定域名，以免产生跨域安全问题
-		corsConfiguration.addAllowedOrigin("*");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addAllowedMethod("*");
-		return corsConfiguration;
-	}
-}
+//@Configuration
+//public class CorsConfig {
+//
+//
+//	@Bean
+//	public CorsWebFilter corsFilter(){
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+//		source.registerCorsConfiguration("/**", buildConfig());
+//		return new CorsWebFilter(source);
+//	}
+//
+//	private CorsConfiguration buildConfig(){
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		//在生产环境上最好指定域名，以免产生跨域安全问题
+//		corsConfiguration.addAllowedOrigin("*");
+//		corsConfiguration.addAllowedHeader("*");
+//		corsConfiguration.addAllowedMethod("*");
+//		return corsConfiguration;
+//	}
+//}
