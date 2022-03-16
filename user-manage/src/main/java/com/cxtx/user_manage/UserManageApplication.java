0f628bff0c911013188fa075f7a5547d28976aa6
sliu <1622436253@qq.com>
2022-03-16 14:22:53 +0800

    seata

--- user-manage/src/main/java/com/cxtx/user_manage/UserManageApplication.java
+++ user-manage/src/main/java/com/cxtx/user_manage/UserManageApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.cxtx.*"})
 @EnableFeignClients
 @MapperScan("com.cxtx.**.mapper")
-@EnableAutoConfiguration
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//这里排除掉
 public class UserManageApplication {
 
     public static void main(String [] args){
